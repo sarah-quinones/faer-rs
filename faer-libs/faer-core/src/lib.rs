6ca9fb6ea2041f886a5c55055123a9180c3f0a2d
Ed Younis <edyounis123@gmail.com>
2024-02-02 20:57:12 -0500

    Sum impl (#93)
    * Init impl with failing test
    
    * Implemented tree-sum

--- faer-libs/faer-core/src/lib.rs
+++ faer-libs/faer-core/src/lib.rs
@@ -4891,6 +4891,15 @@ const __COL_IMPL: () = {
             norm_l2((*self).rb().as_2d())
         }
 
+        /// Returns the sum of `self`.
+        #[inline]
+        pub fn sum(&self) -> E
+        where
+            E: ComplexField,
+        {
+            sum((*self).rb().as_2d())
+        }
+
         /// Returns a view over the matrix.
         #[inline]
         pub fn as_ref(&self) -> ColRef<'_, E> {
@@ -5323,6 +5332,15 @@ const __COL_IMPL: () = {
             norm_l2((*self).rb().as_2d())
         }
 
+        /// Returns the sum of `self`.
+        #[inline]
+        pub fn sum(&self) -> E
+        where
+            E: ComplexField,
+        {
+            sum((*self).rb().as_2d())
+        }
+
         /// Returns a view over the matrix.
         #[inline]
         pub fn as_ref(&self) -> ColRef<'_, E> {
@@ -5683,6 +5701,15 @@ const __ROW_IMPL: () = {
             E: ComplexField,
         {
             norm_l2((*self).rb().as_2d())
+        }
+
+        /// Returns the sum of `self`.
+        #[inline]
+        pub fn sum(&self) -> E
+        where
+            E: ComplexField,
+        {
+            sum((*self).rb().as_2d())
         }
 
         /// Returns a view over the matrix.
@@ -6091,6 +6118,15 @@ const __ROW_IMPL: () = {
             E: ComplexField,
         {
             norm_l2((*self).rb().as_2d())
+        }
+
+        /// Returns the sum of `self`.
+        #[inline]
+        pub fn sum(&self) -> E
+        where
+            E: ComplexField,
+        {
+            sum((*self).rb().as_2d())
         }
 
         /// Returns a view over the matrix.
@@ -6989,6 +7025,15 @@ const __MAT_IMPL: () = {
             E: ComplexField,
         {
             norm_l2((*self).rb())
+        }
+
+        /// Returns the sum of `self`.
+        #[inline]
+        pub fn sum(&self) -> E
+        where
+            E: ComplexField,
+        {
+            sum((*self).rb())
         }
 
         /// Returns a view over the matrix.
@@ -8158,6 +8203,15 @@ const __MAT_IMPL: () = {
             norm_l2((*self).rb())
         }
 
+        /// Returns the sum of `self`.
+        #[inline]
+        pub fn sum(&self) -> E
+        where
+            E: ComplexField,
+        {
+            sum((*self).rb())
+        }
+
         /// Returns a view over the matrix.
         #[inline]
         pub fn as_ref(&self) -> MatRef<'_, E> {
@@ -9191,6 +9245,15 @@ impl<E: Entity> Col<E> {
     {
         norm_l2((*self).as_ref().as_2d())
     }
+
+    /// Returns the sum of `self`.
+    #[inline]
+    pub fn sum(&self) -> E
+    where
+        E: ComplexField,
+    {
+        sum((*self).as_ref().as_2d())
+    }
 }
 
 impl<E: Entity> Row<E> {
@@ -9674,6 +9737,15 @@ impl<E: Entity> Row<E> {
         E: ComplexField,
     {
         norm_l2((*self).as_ref().as_2d())
+    }
+    
+    /// Returns the sum of `self`.
+    #[inline]
+    pub fn sum(&self) -> E
+    where
+        E: ComplexField,
+    {
+        sum((*self).as_ref().as_2d())
     }
 }
 
@@ -10309,6 +10381,15 @@ impl<E: Entity> Mat<E> {
         E: ComplexField,
     {
         norm_l2((*self).as_ref())
+    }
+
+    /// Returns the sum of `self`.
+    #[inline]
+    pub fn sum(&self) -> E
+    where
+        E: ComplexField,
+    {
+        sum((*self).as_ref())
     }
 
     /// Returns an iterator that provides successive chunks of the columns of a view over this
@@ -12028,6 +12109,51 @@ fn norm_l2_with_simd_and_offset_prologue<E: ComplexField, S: pulp::Simd>(
 }
 
 #[inline(always)]
+fn sum_with_simd_and_offset_prologue<E: ComplexField, S: pulp::Simd>(
+    simd: S,
+    data: SliceGroup<'_, E>,
+    offset: pulp::Offset<SimdMaskFor<E, S>>,
+) -> SimdGroupFor<E, S> {
+    use group_helpers::*;
+    
+    let simd = SimdFor::<E, S>::new(simd);
+
+    let zero = simd.splat(E::faer_zero());
+
+    let mut acc0 = zero;
+    let mut acc1 = zero;
+    let mut acc2 = zero;
+    let mut acc3 = zero;
+    let (head, body, tail) = simd.as_aligned_simd(data, offset);
+    let (body4, body1) = body.as_arrays::<4>();
+    let head = head.read_or(zero);
+    acc0 = simd.add(acc0, head);
+
+    for [x0, x1, x2, x3] in body4.into_ref_iter().map(RefGroup::unzip) {
+        let x0 = x0.get();
+        let x1 = x1.get();
+        let x2 = x2.get();
+        let x3 = x3.get();
+        acc0 = simd.add(acc0, x0);
+        acc1 = simd.add(acc1, x1);
+        acc2 = simd.add(acc2, x2);
+        acc3 = simd.add(acc3, x3);
+    }
+
+    for x0 in body1.into_ref_iter() {
+        let x0 = x0.get();
+        acc0 = simd.add(acc0, x0);
+    }
+
+    let tail = tail.read_or(zero);
+    acc3 = simd.add(acc3, tail);
+    
+    acc0 = simd.add(acc0, acc1);
+    acc2 = simd.add(acc2, acc3);
+    simd.add(acc0, acc2)
+}
+
+#[inline(always)]
 fn norm_max_contiguous<E: RealField>(data: MatRef<'_, E>) -> E {
     struct Impl<'a, E: RealField> {
         data: MatRef<'a, E>,
@@ -12170,6 +12296,57 @@ fn norm_l2_with_simd_and_offset_pairwise_rows<E: ComplexField, S: Simd>(
 }
 
 #[inline(always)]
+fn sum_with_simd_and_offset_pairwise_rows<E: ComplexField, S: Simd>(
+    simd: S,
+    data: SliceGroup<'_, E>,
+    offset: pulp::Offset<SimdMaskFor<E, S>>,
+    last_offset: pulp::Offset<SimdMaskFor<E, S>>,
+) -> SimdGroupFor<E, S> {
+    struct Impl<'a, E: ComplexField, S: Simd> {
+        simd: S,
+        data: SliceGroup<'a, E>,
+        offset: pulp::Offset<SimdMaskFor<E, S>>,
+        last_offset: pulp::Offset<SimdMaskFor<E, S>>,
+    }
+
+    impl<E: ComplexField, S: Simd> pulp::NullaryFnOnce for Impl<'_, E, S> {
+        type Output = SimdGroupFor<E, S>;
+
+        #[inline(always)]
+        fn call(self) -> Self::Output {
+            let Self {
+                simd,
+                data,
+                offset,
+                last_offset,
+            } = self;
+
+            if data.len() == NORM_L2_THRESHOLD {
+                sum_with_simd_and_offset_prologue(simd, data, offset)
+            } else if data.len() < NORM_L2_THRESHOLD {
+                sum_with_simd_and_offset_prologue(simd, data, last_offset)
+            } else {
+                let split_point = ((data.len() + 1) / 2).next_power_of_two();
+                let (head, tail) = data.split_at(split_point);
+                let acc0 = sum_with_simd_and_offset_pairwise_rows(simd, head, offset, last_offset);
+                let acc1 = sum_with_simd_and_offset_pairwise_rows(simd, tail, offset, last_offset);
+
+                use group_helpers::*;
+                let simd = SimdFor::<E, S>::new(simd);
+                simd.add(acc0, acc1)
+            }
+        }
+    }
+
+    simd.vectorize(Impl {
+        simd,
+        data,
+        offset,
+        last_offset,
+    })
+}
+
+#[inline(always)]
 fn norm_l2_with_simd_and_offset_pairwise_cols<E: ComplexField, S: Simd>(
     simd: S,
     data: MatRef<'_, E>,
@@ -12239,6 +12416,62 @@ fn norm_l2_with_simd_and_offset_pairwise_cols<E: ComplexField, S: Simd>(
     })
 }
 
+#[inline(always)]
+fn sum_with_simd_and_offset_pairwise_cols<E: ComplexField, S: Simd>(
+    simd: S,
+    data: MatRef<'_, E>,
+    offset: pulp::Offset<SimdMaskFor<E, S>>,
+    last_offset: pulp::Offset<SimdMaskFor<E, S>>,
+) -> SimdGroupFor<E, S> {
+    struct Impl<'a, E: ComplexField, S: Simd> {
+        simd: S,
+        data: MatRef<'a, E>,
+        offset: pulp::Offset<SimdMaskFor<E, S>>,
+        last_offset: pulp::Offset<SimdMaskFor<E, S>>,
+    }
+
+    impl<E: ComplexField, S: Simd> pulp::NullaryFnOnce for Impl<'_, E, S> {
+        type Output = SimdGroupFor<E, S>;
+
+        #[inline(always)]
+        fn call(self) -> Self::Output {
+            use group_helpers::*;
+
+            let Self {
+                simd,
+                data,
+                offset,
+                last_offset,
+            } = self;
+            if data.ncols() == 1 {
+                sum_with_simd_and_offset_pairwise_rows(
+                    simd,
+                    SliceGroup::<'_, E>::new(data.try_get_contiguous_col(0)),
+                    offset,
+                    last_offset,
+                )
+            } else {
+                let split_point = (data.ncols() / 2).next_power_of_two();
+
+                let (head, tail) = data.split_at_col(split_point);
+
+                let acc0 = sum_with_simd_and_offset_pairwise_cols(simd, head, offset, last_offset);
+                let acc1 = sum_with_simd_and_offset_pairwise_cols(simd, tail, offset, last_offset);
+
+                let simd = SimdFor::<E, S>::new(simd);
+                simd.add(acc0, acc1)
+            }
+        }
+    }
+
+    simd.vectorize(Impl {
+        simd,
+        data,
+        offset,
+        last_offset,
+    })
+}
+
 fn norm_l2_contiguous<E: ComplexField>(data: MatRef<'_, E>) -> (E::Real, E::Real, E::Real) {
     struct Impl<'a, E: ComplexField> {
         data: MatRef<'a, E>,
@@ -12267,6 +12500,35 @@ fn norm_l2_contiguous<E: ComplexField>(data: MatRef<'_, E>) -> (E::Real, E::Real
                 simd.reduce_add(simd.rotate_left(acc, offset.rotate_left_amount())),
                 simd.reduce_add(simd.rotate_left(acc_big, offset.rotate_left_amount())),
             )
+        }
+    }
+
+    E::Simd::default().dispatch(Impl { data })
+}
+
+fn sum_contiguous<E: ComplexField>(data: MatRef<'_, E>) -> E {
+    struct Impl<'a, E: ComplexField> {
+        data: MatRef<'a, E>,
+    }
+
+    impl<E: ComplexField> pulp::WithSimd for Impl<'_, E> {
+        type Output = E;
+
+        #[inline(always)]
+        fn with_simd<S: pulp::Simd>(self, simd: S) -> Self::Output {
+            let Self { data } = self;
+            use group_helpers::*;
+
+            let offset =
+                SimdFor::<E, S>::new(simd).align_offset_ptr(data.as_ptr(), NORM_L2_THRESHOLD);
+
+            let last_offset = SimdFor::<E, S>::new(simd)
+                .align_offset_ptr(data.as_ptr(), data.nrows() % NORM_L2_THRESHOLD);
+
+            let acc = sum_with_simd_and_offset_pairwise_cols(simd, data, offset, last_offset);
+
+            let simd = SimdFor::<E, S>::new(simd);
+            simd.reduce_add(acc)
         }
     }
 
@@ -12352,6 +12614,35 @@ fn norm_l2<E: ComplexField>(mut mat: MatRef<'_, E>) -> E::Real {
     }
 }
 
+fn sum<E: ComplexField>(mut mat: MatRef<'_, E>) -> E {
+    if mat.ncols() > 1 && mat.col_stride().unsigned_abs() < mat.row_stride().unsigned_abs() {
+        mat = mat.transpose();
+    }
+    if mat.row_stride() < 0 {
+        mat = mat.reverse_rows();
+    }
+
+    if mat.nrows() == 0 || mat.ncols() == 0 {
+        E::faer_zero()
+    } else {
+        let m = mat.nrows();
+        let n = mat.ncols();
+
+        let mut acc = E::faer_zero();
+
+        if mat.row_stride() == 1 {
+            acc = sum_contiguous(mat);
+        } else {
+            for j in 0..n {
+                for i in 0..m {
+                    acc = acc.faer_add(mat.read(i, j));
+                }
+            }
+        }
+
+        acc
+    }
+}
 fn norm_max<E: ComplexField>(mut mat: MatRef<'_, E>) -> E::Real {
     if mat.ncols() > 1 && mat.col_stride().unsigned_abs() < mat.row_stride().unsigned_abs() {
         mat = mat.transpose();
@@ -13424,6 +13715,31 @@ mod tests {
         let mat = Col::from_fn(10000000, |_| 0.3);
         let target = (0.3 * 0.3 * 10000000.0f64).sqrt();
         assert!(relative_err(mat.norm_l2(), target) < 1e-14);
+    }
+
+    #[test]
+    fn test_sum() {
+        let relative_err = |a: f64, b: f64| (a - b).abs() / f64::max(a.abs(), b.abs());
+
+        for (m, n) in [(9, 10), (1023, 5), (42, 1)] {
+            for factor in [0.0, 1.0, 1e30, 1e250, 1e-30, 1e-250] {
+                let mat = Mat::from_fn(m, n, |i, j| factor * ((i + j) as f64));
+                let mut target = 0.0;
+                zipped!(mat.as_ref()).for_each(|unzipped!(x)| {
+                    target += *x;
+                });
+
+                if factor == 0.0 {
+                    assert!(mat.sum() == target);
+                } else {
+                    assert!(relative_err(mat.sum(), target) < 1e-14);
+                }
+            }
+        }
+
+        let mat = Col::from_fn(10000000, |_| 0.3);
+        let target = 0.3 * 10000000.0f64;
+        assert!(relative_err(mat.sum(), target) < 1e-14);
     }
 
     #[test]
