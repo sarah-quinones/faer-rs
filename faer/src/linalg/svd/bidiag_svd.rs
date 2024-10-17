549bee4b2e3c0da93766a1d1bb58c5cfe0e3638f
sarah quiñones <sarah@veganb.tw>
2024-10-17 18:16:36 +0200

    [wip] bidiag svd

--- faer/src/linalg/svd/bidiag_svd.rs
+++ faer/src/linalg/svd/bidiag_svd.rs
@@ -22,7 +22,6 @@ pub enum SvdError {
 }
 
 #[math]
-#[cfg(test)]
 fn bidiag_to_mat<'N, C: RealContainer, T: RealField<C>>(
     diag: ColRef<'_, C, T, Dim<'N>, ContiguousFwd>,
     subdiag: ColRef<'_, C, T, Dim<'N>, ContiguousFwd>,
@@ -158,7 +157,7 @@ fn qr_algorithm<'N, C: RealContainer, T: RealField<C>>(
 
             let mu;
             if math(t01_2 > sml) {
-                let d = math((t00 - t11) * from_f64(0.5));
+                let d = math(mul_pow2((t00 - t11), from_f64(0.5)));
                 let mut delta = math(sqrt(abs2(d) + t01_2));
                 if math(lt_zero(d)) {
                     delta = math(-delta);
@@ -284,13 +283,272 @@ fn qr_algorithm<'N, C: RealContainer, T: RealField<C>>(
     Ok(())
 }
 
+#[math]
+fn secular_eq<'N, C: RealContainer, T: RealField<C>>(
+    ctx: &Ctx<C, T>,
+    mu: C::Of<T>,
+    col0_perm: ColRef<'_, C, T, Dim<'N>, ContiguousFwd>,
+    diag_perm: ColRef<'_, C, T, Dim<'N>, ContiguousFwd>,
+    shift: C::Of<T>,
+) -> C::Of<T> {
+    let mut res = math(one());
+
+    let n = diag_perm.nrows();
+    for i in n.indices() {
+        let c = math(col0_perm[i]);
+        let d = math(diag_perm[i]);
+        res = math(res + (c / ((d - shift) - mu)) * (c / ((d + shift) + mu)));
+    }
+
+    res
+}
+
+#[math]
+fn deflate<'N, C: RealContainer, T: RealField<C>>(
+    ctx: &Ctx<C, T>,
+    diag: ColMut<'_, C, T, Dim<'N>, ContiguousFwd>,
+    col0: ColMut<'_, C, T, Dim<'N>, ContiguousFwd>,
+    jacobi_coeff: &mut [JacobiRotation<C, T>],
+    jacobi_idx: &mut [Idx<'N>],
+    transpositions: &mut [Idx<'N>],
+    perm: &mut [Idx<'N>],
+    k: usize,
+    stack: &mut DynStack,
+) -> (usize, usize) {
+    help!(C);
+    let mut diag = diag;
+    let mut col0 = col0;
+
+    let n = diag.nrows();
+    let first = n.idx(0);
+
+    let mut jacobi_0i = 0;
+    let mut jacobi_ij = 0;
+
+    let (max_diag, max_col0) = (
+        diag.rb()
+            .subrows_range((first.next(), n.end()))
+            .norm_max_with(ctx),
+        col0.norm_max_with(ctx),
+    );
+    let max = math(max(max_diag, max_col0));
+
+    let eps = math(eps());
+    let sml = math(min_positive());
+
+    let eps_strict = math(max(eps * max_diag, sml));
+
+    let eps_coarse = math(from_f64(8.0) * eps * max);
+
+    // condition 4.1
+    if math(diag[first] < eps_coarse) {
+        write1!(diag[first] = math.copy(eps_coarse));
+        write1!(col0[first] = math.copy(eps_coarse));
+    }
+
+    // condition 4.2
+    for i in first.next().to(n.end()) {
+        if math(abs(col0[i]) < eps_strict) {
+            write1!(col0[i] = math(zero()));
+        }
+    }
+
+    // condition 4.3
+    for i in first.next().to(n.end()) {
+        if math(diag[i] < eps_coarse) {
+            if let Some(rot) = deflation_43(ctx, diag.rb_mut(), col0.rb_mut(), i) {
+                jacobi_coeff[jacobi_0i] = rot;
+                jacobi_idx[jacobi_0i] = i;
+                jacobi_0i += 1;
+            }
+        }
+    }
+
+    let mut total_deflation = true;
+    for i in first.next().to(n.end()) {
+        if math(!(abs(col0[i]) < sml)) {
+            total_deflation = false;
+            break;
+        }
+    }
+
+    perm[0] = first;
+    let mut p = 1;
+    for i in first.next().to(n.end()) {
+        if math(abs(diag[i]) < sml) {
+            perm[p] = i;
+            p += 1;
+        }
+    }
+
+    let mut i = 1;
+    let mut j = k + 1;
+
+    for p in &mut perm[p..] {
+        if i >= k + 1 {
+            *p = n.idx(j);
+            j += 1;
+        } else if j >= *n {
+            *p = n.idx(i);
+            i += 1;
+        } else if math(diag[n.idx(i)] < diag[n.idx(j)]) {
+            *p = n.idx(j);
+            j += 1;
+        } else {
+            *p = n.idx(i);
+            i += 1;
+        }
+    }
+
+    if total_deflation {
+        for i in first.next().to(n.end()) {
+            let pi = perm[*i];
+            if math(abs(diag[pi]) < sml || diag[pi] > diag[first]) {
+                perm[*i - 1] = perm[*i];
+            } else {
+                perm[*i - 1] = first;
+                break;
+            }
+        }
+    }
+
+    let (mut real_ind, stack) = stack.collect(n.indices());
+    let (mut real_col, _) = stack.collect(n.indices());
+
+    for i in (if total_deflation {
+        zero()
+    } else {
+        first.next()
+    })
+    .to(n.end())
+    {
+        let pi = perm[*n - if total_deflation { *i + 1 } else { *i }];
+        let j = real_col[*pi];
+
+        let (a, b) = math((copy(diag[i]), copy(diag[j])));
+        write1!(diag[i] = b);
+        write1!(diag[j] = a);
+
+        if *i != 0 && *j != 0 {
+            let (a, b) = math((copy(col0[i]), copy(col0[j])));
+            write1!(col0[i] = b);
+            write1!(col0[j] = a);
+        }
+
+        transpositions[*i] = j;
+        let real_i = real_ind[*i];
+        real_col[*real_i] = j;
+        real_col[*pi] = i;
+        real_ind[*j] = real_i;
+        real_ind[*i] = pi;
+    }
+
+    write1!(col0[first] = math(copy(diag[first])));
+
+    for i in n.indices() {
+        perm[*i] = i;
+    }
+
+    for (i, &j) in transpositions.iter().enumerate() {
+        perm.swap(i, *j);
+    }
+
+    // condition 4.4
+    let mut i = n.idx(*n - 1);
+    while *i > 0 && math((abs(diag[i]) < sml || abs(col0[i]) < sml)) {
+        i = n.idx(*i - 1);
+    }
+
+    while *i > 1 {
+        let i1 = n.idx(*i - 1);
+        if math(diag[i] - diag[i1] < eps_strict) {
+            if let Some(rot) = deflation_44(ctx, diag.rb_mut(), col0.rb_mut(), i1, i) {
+                jacobi_coeff[jacobi_0i + jacobi_ij] = rot;
+                jacobi_idx[jacobi_0i + jacobi_ij] = i;
+                jacobi_ij += 1;
+            }
+        }
+
+        i = i1;
+    }
+
+    (jacobi_0i, jacobi_ij)
+}
+
+#[math]
+fn deflation_43<'N, C: RealContainer, T: RealField<C>>(
+    ctx: &Ctx<C, T>,
+    diag: ColMut<'_, C, T, Dim<'N>, ContiguousFwd>,
+    col0: ColMut<'_, C, T, Dim<'N>, ContiguousFwd>,
+    i: Idx<'N>,
+) -> Option<JacobiRotation<C, T>> {
+    let mut diag = diag;
+    let mut col0 = col0;
+
+    let n = diag.nrows();
+    let first = n.idx(0);
+    help!(C);
+
+    let p = math(copy(col0[first]));
+    let q = math(copy(col0[i]));
+
+    if math(is_zero(p) && is_zero(q)) {
+        write1!(diag[i] = math(zero()));
+        return None;
+    }
+
+    let rot = JacobiRotation::make_givens(ctx, math.copy(p), math.copy(q));
+
+    let r = math(rot.c * p - rot.s * q);
+    write1!(col0[first] = math.copy(r));
+    write1!(diag[first] = math.copy(r));
+    write1!(col0[i] = math.zero());
+    write1!(diag[i] = math.zero());
+
+    Some(rot)
+}
+
+#[math]
+fn deflation_44<'N, C: RealContainer, T: RealField<C>>(
+    ctx: &Ctx<C, T>,
+    diag: ColMut<'_, C, T, Dim<'N>, ContiguousFwd>,
+    col0: ColMut<'_, C, T, Dim<'N>, ContiguousFwd>,
+    i: Idx<'N>,
+    j: Idx<'N>,
+) -> Option<JacobiRotation<C, T>> {
+    let mut diag = diag;
+    let mut col0 = col0;
+
+    help!(C);
+
+    let p = math(copy(col0[i]));
+    let q = math(copy(col0[j]));
+
+    if math(is_zero(p) && is_zero(q)) {
+        write1!(diag[i] = math(copy(diag[j])));
+        return None;
+    }
+
+    let rot = JacobiRotation::make_givens(ctx, math.copy(p), math.copy(q));
+
+    let r = math(rot.c * p - rot.s * q);
+    write1!(col0[i] = math.copy(r));
+    write1!(col0[j] = math.zero());
+    write1!(diag[i] = math(copy(diag[j])));
+
+    Some(rot)
+}
+
 #[cfg(test)]
 mod tests {
     use faer_traits::Unit;
 
     use super::*;
-    use crate::{assert, utils::approx::*, Col, Mat};
-    use std::path::{Path, PathBuf};
+    use crate::{assert, utils::approx::*, Col, Mat, MatMut};
+    use std::{
+        mem::MaybeUninit,
+        path::{Path, PathBuf},
+    };
 
     #[math]
     fn bidiag_to_mat<'N>(
@@ -406,5 +664,231 @@ mod tests {
 
             assert!(reconstructed ~ bidiag_to_mat(diag, subdiag));
         }
+    }
+
+    #[test]
+    fn test_deflation43() {
+        let approx_eq = CwiseMat(ApproxEq::<Unit, f32>::eps());
+
+        let n = 7;
+        let k = 3;
+        let diag_orig = &*vec![1.0, 5.0, 3.0, 1e-7, 4.0, 2.0, 2e-7_f32];
+        let col_orig = &*vec![1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0_f32];
+
+        with_dim!(n, n);
+
+        let n_jacobi = 2;
+        let jacobi_coeffs = &mut *vec![JacobiRotation { c: 0.0, s: 0.0 }; n_jacobi];
+        let jacobi_indices = &mut *vec![n.idx(0); n_jacobi];
+        let perm = &mut *vec![n.idx(0); *n];
+        let diag = &mut *(diag_orig.to_vec());
+        let col = &mut *(col_orig.to_vec());
+
+        let mut diag = MatMut::from_column_major_slice_mut(diag, n, 1)
+            .col_mut(0)
+            .try_as_col_major_mut()
+            .unwrap();
+        let mut col = MatMut::from_column_major_slice_mut(col, n, 1)
+            .col_mut(0)
+            .try_as_col_major_mut()
+            .unwrap();
+
+        let (jacobi_0i, jacobi_ij) = deflate(
+            &ctx(),
+            diag.rb_mut(),
+            col.rb_mut(),
+            jacobi_coeffs,
+            jacobi_indices,
+            &mut *vec![n.idx(0); *n],
+            perm,
+            k,
+            DynStack::new_any(&mut *vec![MaybeUninit::new(0usize); 2 * *n]),
+        );
+        assert!(all(jacobi_0i == 2, jacobi_ij == 0));
+
+        let perm_inv = &mut *vec![n.idx(0); *n];
+        for (i, &p) in perm.iter().enumerate() {
+            perm_inv[*p] = n.idx(i);
+        }
+
+        let P = crate::perm::PermRef::new_checked(perm, perm_inv, n);
+
+        let mut M_orig = Mat::zeros_with(&ctx(), n, n);
+        for i in n.indices() {
+            M_orig[(i, i)] = diag_orig[*i];
+            M_orig[(i, n.idx(0))] = col_orig[*i];
+        }
+
+        let mut M = Mat::zeros_with(&ctx(), n, n);
+        for i in n.indices() {
+            M[(i, i)] = diag[i];
+            M[(i, n.idx(0))] = col[i];
+        }
+
+        M = P.inverse() * M * P;
+
+        for (&rot, &i) in core::iter::zip(&*jacobi_coeffs, &*jacobi_indices).rev() {
+            let (x, y) = M.two_rows_mut(n.idx(0), i);
+            rot.apply_on_the_left_in_place(&ctx(), x, y);
+        }
+
+        assert!(M ~ M_orig);
+    }
+
+    #[test]
+    fn test_deflation44() {
+        let approx_eq = CwiseMat(ApproxEq::<Unit, f32>::eps());
+
+        let n = 7;
+        let k = 3;
+        let diag_orig = &*vec![1.0, 5.0, 3.0, 1.0, 4.0, 2.0, 1.0];
+        let col_orig = &*vec![1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0_f32];
+
+        with_dim!(n, n);
+
+        let n_jacobi = 1;
+        let jacobi_coeffs = &mut *vec![JacobiRotation { c: 0.0, s: 0.0 }; n_jacobi];
+        let jacobi_indices = &mut *vec![n.idx(0); n_jacobi];
+        let perm = &mut *vec![n.idx(0); *n];
+        let diag = &mut *(diag_orig.to_vec());
+        let col = &mut *(col_orig.to_vec());
+
+        let mut diag = MatMut::from_column_major_slice_mut(diag, n, 1)
+            .col_mut(0)
+            .try_as_col_major_mut()
+            .unwrap();
+        let mut col = MatMut::from_column_major_slice_mut(col, n, 1)
+            .col_mut(0)
+            .try_as_col_major_mut()
+            .unwrap();
+
+        let (jacobi_0i, jacobi_ij) = deflate(
+            &ctx(),
+            diag.rb_mut(),
+            col.rb_mut(),
+            jacobi_coeffs,
+            jacobi_indices,
+            &mut *vec![n.idx(0); *n],
+            perm,
+            k,
+            DynStack::new_any(&mut *vec![MaybeUninit::new(0usize); 2 * *n]),
+        );
+        assert!(all(jacobi_0i == 0, jacobi_ij == 1));
+
+        let perm_inv = &mut *vec![n.idx(0); *n];
+        for (i, &p) in perm.iter().enumerate() {
+            perm_inv[*p] = n.idx(i);
+        }
+
+        let P = crate::perm::PermRef::new_checked(perm, perm_inv, n);
+
+        let mut M_orig = Mat::zeros_with(&ctx(), n, n);
+        for i in n.indices() {
+            M_orig[(i, i)] = diag_orig[*i];
+            M_orig[(i, n.idx(0))] = col_orig[*i];
+        }
+
+        let mut M = Mat::zeros_with(&ctx(), n, n);
+        for i in n.indices() {
+            M[(i, i)] = diag[i];
+            M[(i, n.idx(0))] = col[i];
+        }
+
+        M = P.inverse() * M * P;
+
+        for (&rot, &i) in core::iter::zip(&*jacobi_coeffs, &*jacobi_indices).rev() {
+            let (i, j) = (n.idx(*i - 1), i);
+            let (pi, pj) = (perm[*i], perm[*j]);
+
+            let (x, y) = M.two_rows_mut(pi, pj);
+            rot.apply_on_the_left_in_place(&ctx(), x, y);
+
+            let (x, y) = M.two_cols_mut(pi, pj);
+            rot.transpose(&ctx())
+                .apply_on_the_right_in_place(&ctx(), x, y);
+        }
+
+        assert!(M ~ M_orig);
+    }
+
+    #[test]
+    fn test_both_deflation() {
+        let approx_eq = CwiseMat(ApproxEq::<Unit, f32>::eps());
+
+        let n = 7;
+        let k = 3;
+        let diag_orig = &*vec![1.0, 5.0, 3.0, 2.0, 4.0, 2.0, 0.0];
+        let col_orig = &*vec![1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0_f32];
+
+        with_dim!(n, n);
+
+        let n_jacobi = 2;
+        let jacobi_coeffs = &mut *vec![JacobiRotation { c: 0.0, s: 0.0 }; n_jacobi];
+        let jacobi_indices = &mut *vec![n.idx(0); n_jacobi];
+        let perm = &mut *vec![n.idx(0); *n];
+        let diag = &mut *(diag_orig.to_vec());
+        let col = &mut *(col_orig.to_vec());
+
+        let mut diag = MatMut::from_column_major_slice_mut(diag, n, 1)
+            .col_mut(0)
+            .try_as_col_major_mut()
+            .unwrap();
+        let mut col = MatMut::from_column_major_slice_mut(col, n, 1)
+            .col_mut(0)
+            .try_as_col_major_mut()
+            .unwrap();
+
+        let (jacobi_0i, jacobi_ij) = deflate(
+            &ctx(),
+            diag.rb_mut(),
+            col.rb_mut(),
+            jacobi_coeffs,
+            jacobi_indices,
+            &mut *vec![n.idx(0); *n],
+            perm,
+            k,
+            DynStack::new_any(&mut *vec![MaybeUninit::new(0usize); 2 * *n]),
+        );
+        assert!(all(jacobi_0i == 1, jacobi_ij == 1));
+
+        let perm_inv = &mut *vec![n.idx(0); *n];
+        for (i, &p) in perm.iter().enumerate() {
+            perm_inv[*p] = n.idx(i);
+        }
+
+        let P = crate::perm::PermRef::new_checked(perm, perm_inv, n);
+
+        let mut M_orig = Mat::zeros_with(&ctx(), n, n);
+        for i in n.indices() {
+            M_orig[(i, i)] = diag_orig[*i];
+            M_orig[(i, n.idx(0))] = col_orig[*i];
+        }
+
+        let mut M = Mat::zeros_with(&ctx(), n, n);
+        for i in n.indices() {
+            M[(i, i)] = diag[i];
+            M[(i, n.idx(0))] = col[i];
+        }
+
+        M = P.inverse() * M * P;
+
+        for (&rot, &i) in core::iter::zip(&jacobi_coeffs[1..], &jacobi_indices[1..]).rev() {
+            let (i, j) = (n.idx(*i - 1), i);
+            let (pi, pj) = (perm[*i], perm[*j]);
+
+            let (x, y) = M.two_rows_mut(pi, pj);
+            rot.apply_on_the_left_in_place(&ctx(), x, y);
+
+            let (x, y) = M.two_cols_mut(pi, pj);
+            rot.transpose(&ctx())
+                .apply_on_the_right_in_place(&ctx(), x, y);
+        }
+
+        for (&rot, &i) in core::iter::zip(&jacobi_coeffs[..1], &jacobi_indices[..1]).rev() {
+            let (x, y) = M.two_rows_mut(n.idx(0), i);
+            rot.apply_on_the_left_in_place(&ctx(), x, y);
+        }
+
+        assert!(M ~ M_orig);
     }
 }
