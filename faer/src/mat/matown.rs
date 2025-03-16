06ade7b83cfa6f612be0ec0e05a0604df9b307f8
Markus <markushh@uio.no>
2025-03-16 15:07:21 +0100

    Implement insert_row and insert_col
    These are helper functions that utilize resize_with to add a row or column to the matrix

--- faer/src/mat/matown.rs
+++ faer/src/mat/matown.rs
@@ -1399,6 +1399,44 @@ impl<'short, T, Rows: Shape, Cols: Shape> ReborrowMut<'short> for Mat<T, Rows, C
 	}
 }
 
+impl<T, Cols: Shape> Mat<T, usize, Cols> {
+	/// inserts a row at the end of the matrix
+	/// # panics
+	/// The function panics if the number of columns in the row does not match the number of columns in the matrix
+	pub fn insert_row(&mut self, row: RowRef<'_, T, Cols>)
+	where
+		T: Clone,
+	{
+		self::assert!(
+			self.ncols() == row.ncols(),
+			"row ncols ({:?}) must match matrix ncols ({:?})",
+			row.ncols(),
+			self.ncols()
+		);
+
+		self.resize_with(self.nrows() + 1, self.ncols(), |_, j| row[j].clone());
+	}
+}
+
+impl<T, Rows: Shape> Mat<T, Rows, usize> {
+	/// inserts a col at the end of the matrix
+	/// # panics
+	/// The function panics if the number of rows in the col does not match the number of rows in the matrix
+	pub fn insert_col(&mut self, col: ColRef<'_, T, Rows>)
+	where
+		T: Clone,
+	{
+		self::assert!(
+			self.nrows() == col.nrows(),
+			"col nrows ({:?}) must match matrix nrows ({:?})",
+			col.nrows(),
+			self.nrows()
+		);
+
+		self.resize_with(self.nrows(), self.ncols() + 1, |i, _| col[i].clone());
+	}
+}
+
 #[cfg(test)]
 mod tests {
 	use crate::{assert, mat};
@@ -1478,5 +1516,52 @@ mod tests {
 		];
 
 		assert!(m == target);
+	}
+
+	#[test]
+	fn test_insert_row() {
+		let mut m = mat![[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0],];
+
+		let row = row![10.0, 11.0, 12.0];
+
+		m.insert_row(row.as_ref());
+
+		let target = mat![[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0], [10.0, 11.0, 12.0],];
+
+		assert!(m == target);
+	}
+
+	#[test]
+	#[should_panic]
+	fn test_insert_row_panic() {
+		let mut m = mat![[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0],];
+
+		// row has one too many columns
+		let row = row![10.0, 11.0, 12.0, 13.0];
+
+		m.insert_row(row.as_ref());
+	}
+
+	#[test]
+	fn test_insert_col() {
+		let mut m = mat![[1.0, 2.0, 3.0], [1.0, 2.0, 3.0], [1.0, 2.0, 3.0],];
+
+		let col = col![4.0, 4.0, 4.0];
+
+		m.insert_col(col.as_ref());
+
+		let target = mat![[1.0, 2.0, 3.0, 4.0], [1.0, 2.0, 3.0, 4.0], [1.0, 2.0, 3.0, 4.0],];
+
+		assert!(m == target);
+	}
+
+	#[test]
+	#[should_panic]
+	fn test_insert_col_panic() {
+		let mut m = mat![[1.0, 2.0, 3.0], [1.0, 2.0, 3.0], [1.0, 2.0, 3.0],];
+
+		let col = col![4.0, 4.0, 4.0, 4.0];
+
+		m.insert_col(col.as_ref());
 	}
 }
